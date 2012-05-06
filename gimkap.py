06afb919516c2dcb2d460cd826bac1356cb96f60
ktec <keithsalisbury@gmail.com>
2012-05-06 22:37:29 +0800

    fixed the lat/long to work correctly

--- gimkap.py
+++ gimkap.py
@@ -19,6 +19,78 @@ def unique(seq, idfun=None):
        result.append(item)
    return result
 
+def downloadwithwget(url,filename):
+	os.system("""
+		wget \
+			 --header='Host: khm0.google.com' \
+			 --header='User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0 FirePHP/0.7.1' \
+			 --header='Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+			 --header='Accept-Language: en-us,en;q=0.5' \
+			 --header='Accept-Encoding: gzip, deflate' \
+			 --header='Connection: keep-alive' \
+			 --header='x-insight: activate' \
+			 --header='If-Modified-Since: Fri, 1 Jan 2010 01:00:00 GMT' \
+			 --header='Cache-Control: max-age=0' \
+		     --no-clobber %s -O %s""" % (url, filename) )
+
+def downloadwithcurl(url,filename):
+	print "Downloadng %s" % url
+	os.system( '''
+		curl -d -S \
+		 --header 'Host: khm0.google.com' \
+		 --header 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0 FirePHP/0.7.1' \
+		 --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+		 --header 'Accept-Language: en-us,en;q=0.5' \
+		 --header 'Accept-Encoding: gzip, deflate' \
+		 --header 'Connection: keep-alive' \
+		 --header 'Cache-Control: max-age=0' \
+		--cookie cookie.txt \
+		"%s" \
+		-o "%s"
+	''' % ( url, filename ) )
+
+
+def download2(url,filename):
+	from urllib2 import Request, urlopen
+	print "Downloadng %s" % url
+	headers = {
+		"Host": "khm0.google.com",
+		"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0 FirePHP/0.7.1",
+		"Accept": "image/png,image/*;q=0.8,*/*;q=0.5",
+		"Accept-Language": "en-us,en;q=0.5",
+		"Accept-Encoding": "gzip, deflate",
+		"Connection": "keep-alive",
+		"Referer": "http://maps.google.com/",
+		"Origin": "http://maps.google.com",
+		"x-insight": "activate"
+	}
+	req = Request(url, None, headers)
+	try:
+		response = urlopen(req)
+	except IOError, e:
+		if hasattr(e, 'reason'):
+        		print 'We failed to reach a server.'
+        		print 'Reason: ', e.reason
+    		elif hasattr(e, 'code'):
+        		print 'The server couldn\'t fulfill the request.'
+        		print 'Error code: ', e.code
+	else:
+    		# everything is fine
+		with open(filename, 'a') as f:
+			f.write(response.read())
+
+def download3(host,path,filename):
+	from urllib3 import connection_from_url, HTTPError, TimeoutError, MaxRetryError
+
+	http_pool = connection_from_url(host, timeout=1.0)
+	try:
+	    r = http_pool.get_url(path, retries=2)
+	except TimeoutError, e:
+	    print "TimeoutError %s" % e.reason
+	except MaxRetryError, e:
+	    print "MaxRetryError"
+
+
 if __name__ == "__main__":
 
 	import sys, os
@@ -32,7 +104,6 @@ if __name__ == "__main__":
 		print "This utility prints for given WGS84 lat/lon coordinates (or bounding box) the list of tiles"
 		print "covering specified area. Tiles are in the given 'profile' (default is Google Maps 'mercator')"
 		print "and in the given pyramid 'zoomlevel'."
-		print "For each tile several information is printed including bonding box in EPSG:900913 and WGS84."
 		sys.exit(1)
 
 	profile = 'mercator'
@@ -85,55 +156,75 @@ if __name__ == "__main__":
 		tmaxx, tmaxy = tminx, tminy
 	
 	tiles = []
-	wgs = []
-
+	#wgs = []
+	
 	for ty in range(tminy, tmaxy+1):
 		for tx in range(tminx, tmaxx+1):
-			tilefilename = "%s/%s/%s" % (tz, tx, ty)
-			print tilefilename, "( TileMapService: z / x / y )"
-		
+			
 			gx, gy = mercator.GoogleTile(tx, ty, tz)
 			print "Google tile: %d, %d" % (gx, gy)
+			
+			dirname = os.path.join(str(tz), str(gx))
+			tilefilename = os.path.join(dirname, "%s.jpg" % gy)
+			print tilefilename, "( TileMapService: z / x / y )"
+		
+			if os.path.isfile(tilefilename):
+				print "File %s exists." % tilefilename
+			else:
+				if not os.path.exists(dirname): os.makedirs(dirname)
+				#gmapsurl = "http://khm0.google.com/kh/v=109&src=app&host=maps.google.com"
+				gmapsurl = "http://khm1.google.co.in/kh/v=109&src=app&s=Galil"
+				url = "%s&x=%d&y=%d&z=%d" % (gmapsurl, gx, gy, tz)
+				downloadwithcurl(url, tilefilename )
+
 			tiles.append((gx, gy))
 			wgsbounds = mercator.TileLatLonBounds( tx, ty, tz)
-			wgs.append(wgsbounds)
+			#print "Tile %s,%s,%s = [%s,%s,%s,%s]" % (tx, ty, tz, wgsbounds[0],wgsbounds[1],wgsbounds[2],wgsbounds[3])
+			#wgs.append(wgsbounds)
 
-	startx = tiles[0][0]
-	starty = tiles[0][1]
-	endx = tiles[len(tiles)-1][0]
-	endy = tiles[len(tiles)-1][1]
+			if ty == tminy and tx == tminx:
+				#print "First item"
+				startx,starty = gx,gy
+				lat,lng = wgsbounds[0],wgsbounds[1]
+			if ty == tmaxy and tx == tmaxx:
+				#print "Last item"
+				endx,endy = gx,gy
+				latmax,lngmax = wgsbounds[2],wgsbounds[3]
+
 
 	print
-	print "bounds [ %f, %f, %f, %f ]" % (wgs[0][0], wgs[0][1], wgs[len(wgs)-1][2], wgs[len(wgs)-1][3])
+	print "bounds [ %f, %f, %f, %f ]" % (lat,lng,latmax,lngmax)
 
-	# apparent curl will automagically create directories as required
-	for x in range(startx, endx+1):
-		os.system("mkdir %d" % x)
-		for y in range(endy, starty+1):
-			fname = "%d/%d.jpg" % (x,y)
-			if os.path.isfile(fname):
-				print "File %s exists." % fname
-			else:
-				print "Download %s " % fname
-				#os.system( "wget -nc -U \"IE6 on Windows XP: Mozilla/4.0 (compatible; MSIE 6.0; Microsoft Windows NT 5.1)\" http://khm1.google.co.in/kh/v=109&src=app&x=%d&y=%d&z=%d&s=Galil -O %s" % (x,y,zoomlevel,fname) )
-			
-	os.system( '''
-curl -d -S --user-agent "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13" --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" --header "Accept-Language: en-US,id-ID;q=0.8,id;q=0.6,en;q=0.4" --header "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3" --header "Keep-Alive: 300" --header "Connection: keep-alive" --cookie cookie.txt "http://khm1.google.co.in/kh/v=109&src=app&x=[%s-%s]&y=[%s-%s]&z=%s&s=Galil" -o "#1/#2.jpg"
-''' % ( startx,endx,endy,starty,zoomlevel) )
+	#print wgsbounds
+	try:
+		print "Stitching job 1"
 
-	print "Stitching job 1"
+		filename = "%i-%s%s-%s%s" % (tz,startx,starty,endx,endy)
+		if not os.path.exists(filename):
+			os.makedirs(filename)
 
-	os.system("for dir in $(ls); do convert $dir/*.jpg -append $dir.jpg;done;")
+		from collections import defaultdict
+		res = defaultdict(list)
+		for v, k in tiles: res[v].append(k)
+		
+		for k,v in res.items():
+			convert = "convert "
+			for l in reversed(v): convert += ("%s/%s/%i.jpg " % (tz, k, l))
+			convert += " -append %s/%s.jpg" % (filename, k)
+			os.system( convert )
 
-	print "Stitching job 2"
+		print "Stitching job 2"
+		os.system("convert %s/*.jpg +append %s.jpg" % (filename,filename) )
 
-	filename = "%s%s-%s%s" % (startx,starty,endx,endy)
-	os.system("convert *.jpg +append %s.jpg" % filename)
+		print
+		print "Map is generated in '%s.jpg'" % filename
+		print "Creating kap file..."
 
-	print "Map is generated in 'map.jpg', you may remove other files"
+		create_kap = "imgkap %s.jpg %f %f %f %f %s.kap" % (filename, latmax,lng,lat,lngmax, filename) 
+		print create_kap
+		os.system( create_kap )
 
-	print "Creating kap file..."
+	except IOError:
+		print "IOError"
 
-	create_kap = "imgkap %s.jpg %f %f %f %f %s.kap" % (filename, wgs[0][0], wgs[0][1], wgs[len(wgs)-1][2], wgs[len(wgs)-1][3], filename) 
-	print create_kap
-	os.system( create_kap )
+
